b3d106caee75a63393569b081cb02965996f1372
Sylvain Savajols <s.savajols@gmail.com>
2018-01-21 23:24:17 +0100

    [EVO] cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import { AppRouter } from './router';
-import { Component1, Component2 } from './modules/contact/contact.module';
-
 
 export function App() {
   return <div>
       <h1>React router app</h1>
       <AppRouter />
-      <Component1 />
-      <Component2 />
   </div>;
 }
--- src/modules/contact/contact.module.tsx
+++ src/modules/contact/contact.module.tsx
@@ -1,9 +1,4 @@
-import * as React from 'react';
-
 export * from './components/contact';
 export * from './components/contact.list';
 export * from './components/contact.form';
 export * from './services/contact.service';
-
-export const Component1 = () => <div>component 1</div>;
-export function Component2() { return <div>component 2</div>;}
